0dab9c908668bc56491cf750f4e31bacae271c52
George <demina.ivann@gmail.com>
2025-02-27 10:36:03 -0800

    Header styling

---
+++ src/components/Header.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import header from "@/styles/modules/header.module.scss"
+export default function Header() {
+  return (
+    <div className={header.container}>
+        <div className={header.iconContainer}>
+            <svg xmlns="http://www.w3.org/2000/svg" className={header.icon} viewBox="0 0 448 448" fill='currentColor'>
+            {/* <!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--> */}
+            <path d="M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z"/>
+            </svg>
+        </div>
+        <div className={header.linkContainer}>
+            <a href="#" className={header.link}>
+                GeorgeHub Shopping
+            </a>
+        </div>
+    </div>
+  )
+}
