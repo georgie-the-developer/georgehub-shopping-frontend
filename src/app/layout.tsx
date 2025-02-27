f1255faad2dc5378764e00a060d7fee6c7e4f6c9
George <demina.ivann@gmail.com>
2025-02-27 10:40:36 -0800

    Add main container for children in app/layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,9 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Header/>
+        <div className="main">
         {children}
+        </div>
       </body>
     </html>
   )
