06b2a4e484e96bba646ebecdb3b8de07765d3af7
George <demina.ivann@gmail.com>
2025-02-28 08:17:06 -0800

    Add cart icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import header from "@/styles/modules/header.module.scss"
+import CartSvg from "/assets/icons/cart.svg"
 export default function Header() {
   return (
     <div className={header.container}>
@@ -9,10 +10,13 @@ export default function Header() {
             <path d="M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z"/>
             </svg>
         </div>
-        <div className={header.linkContainer}>
+        <div className={header.rightContainer}>
             <a href="#" className={header.link}>
                 GeorgeHub Shopping
             </a>
+            <div className={header.iconContainer}>
+              <CartSvg/>
+            </div>
         </div>
     </div>
   )
