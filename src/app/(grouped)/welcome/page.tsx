6c4cb037e3cb9160e9f4dd52ba000331b3f85623
George <demina.ivann@gmail.com>
2025-03-07 08:49:26 -0800

    Welcome page with basic responsiveness

--- src/app/(grouped)/welcome/page.tsx
+++ src/app/(grouped)/welcome/page.tsx
@@ -1,5 +1,17 @@
 import React from "react";
-
+import welcome from "@/styles/modules/welcome.module.scss";
+import ButtonLink from "@/components/buttons/ButtonLink";
 export default function Page() {
-  return <div>Hi there it is welcome page</div>;
+  return (
+    <div className={welcome.container}>
+      <div className={welcome.heading}>Welcome to GeorgeHub Shopping!</div>
+      <div className={welcome.buttonGroup}>
+        <ButtonLink link="/login" text="Login" type="secondary" />
+        <fieldset>
+          <legend>Or</legend>
+        </fieldset>
+        <ButtonLink link="/register" text="Register" type="primary" />
+      </div>
+    </div>
+  );
 }
