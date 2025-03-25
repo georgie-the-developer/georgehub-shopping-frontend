42b38b9616d808cd4e8f5d6a13b14faafaa65414
George <demina.ivann@gmail.com>
2025-03-25 11:36:02 -0700

    Confirm password reset

--- src/app/(grouped)/forgot-password/change-password/page.tsx
+++ src/app/(grouped)/forgot-password/change-password/page.tsx
@@ -3,20 +3,40 @@
 import * as Form from "@/components/form/Form";
 import ButtonLink from "@/components/buttons/ButtonLink";
 import ButtonForm from "@/components/buttons/ButtonForm";
+// Config
+import config from "config.json";
 // Helpers
 import { limitAccesByRole } from "@/helpers/auth-middleware";
+import { getCookie } from "@/helpers/cookies";
 // Styling
 import forgotPassword from "@/styles/modules/login.module.scss";
+import { useSearchParams } from "next/navigation";
 
 export default function Page() {
   limitAccesByRole(["guest"]);
+  let searchParams = useSearchParams();
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const formData = new FormData(e.target);
-    const data = JSON.stringify(Object.fromEntries(formData));
-    //Send verification code...
+    const data = {
+      confirmation_code: e.target["confirm_code"].value,
+      new_password: e.target["new_password"].value,
+      email: searchParams.get("email"),
+    };
+    let url = config.API_URL + "auth/reset-password";
+    let csrf_token = getCookie("csrf_token");
+    let res = await fetch(url, {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "X-CSRFToken": csrf_token,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    let resJson = await res.json();
     console.log(data);
-    e.target.reset();
+    console.log(resJson);
+    // e.target.reset();
   };
   return (
     <div className={forgotPassword.container}>
