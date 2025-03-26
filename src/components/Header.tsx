ad0411213f538faee5c39d075c06f6f260aa1f39
George <demina.ivann@gmail.com>
2025-03-26 11:15:45 -0700

    Show cart icon only if user's role is "buyer"

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import header from "@/styles/modules/header.module.scss";
 import MenuSvg from "@/assets/icons/menu.svg";
 import CartSvg from "@/assets/icons/cart.svg";
+import { useUser } from "@/contexts/UserContext";
 export default function Header({ openSidebar }) {
+  const { user } = useUser();
   return (
     <div className={header.container}>
       <div className={header.iconContainer} onClick={openSidebar}>
@@ -12,10 +14,14 @@ export default function Header({ openSidebar }) {
         <a href="#" className={header.link}>
           GeorgeHub Shopping
         </a>
-        <div className={header.iconContainer}>
-          <CartSvg />
-          <span className={header.dot}></span>
-        </div>
+        {user.role == "buyer" ? (
+          <div className={header.iconContainer}>
+            <CartSvg />
+            {/* <span className={header.dot}></span> */}
+          </div>
+        ) : (
+          ""
+        )}
       </div>
     </div>
   );
