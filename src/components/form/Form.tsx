1ba66fabd0a523cb1e534c38ee0ec9939afd50a1
George <demina.ivann@gmail.com>
2025-03-26 09:12:45 -0700

    Add pending style to form submit button

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -25,6 +25,7 @@ type FormLinkType = {
 
 type FormSubmitType = {
   value: string;
+  isPending: boolean;
 };
 
 // Main component
@@ -106,16 +107,22 @@ export function FormLink({ title, link }: FormLinkType) {
 }
 
 // submit component
-export function FormSubmit({ value }: FormSubmitType) {
+export function FormSubmit({ value, isPending }: FormSubmitType) {
   const { isSubmittable } = useForm();
   return (
-    <div className={form.submitContainer}>
+    <div className={`${form.submitContainer} ${isPending && form.pending}`}>
       <input
         type="submit"
         value={value}
         className={`${form.button} ${!isSubmittable && form.disabeled}`}
         disabled={!isSubmittable}
       />
+      <div className={form.spinnerContainer}>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+          {/* <!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--> */}
+          <path d="M222.7 32.1c5 16.9-4.6 34.8-21.5 39.8C121.8 95.6 64 169.1 64 256c0 106 86 192 192 192s192-86 192-192c0-86.9-57.8-160.4-137.1-184.1c-16.9-5-26.6-22.9-21.5-39.8s22.9-26.6 39.8-21.5C434.9 42.1 512 140 512 256c0 141.4-114.6 256-256 256S0 397.4 0 256C0 140 77.1 42.1 182.9 10.6c16.9-5 34.8 4.6 39.8 21.5z" />
+        </svg>
+      </div>
     </div>
   );
 }
