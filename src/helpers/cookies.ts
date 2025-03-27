3bb50588e10469e04c22f4d184279f9dd0d78eb1
George <demina.ivann@gmail.com>
2025-03-27 11:10:13 -0700

    Even deeper debugging

--- src/helpers/cookies.ts
+++ src/helpers/cookies.ts
@@ -1,7 +1,10 @@
 import config from "config.json";
 export function getCookie(name: string) {
+  console.log(name);
   const value: string = `; ${document.cookie}`;
+  console.log(value);
   const parts: string[] = value.split(`; ${name}=`);
+  console.log(parts);
   if (parts.length === 2) return parts.pop().split(";").shift();
 }
 export async function setCsrf() {
