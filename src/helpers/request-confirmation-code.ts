0216fc2f5c253b19346aa24d31b7b488b04cf89b
George <demina.ivann@gmail.com>
2025-03-27 10:25:23 -0700

    Temporary console.logs for debug info

--- src/helpers/request-confirmation-code.ts
+++ src/helpers/request-confirmation-code.ts
@@ -3,6 +3,8 @@ import { getCookie } from "./cookies";
 export const requestConfirmCode = async (email: string) => {
   let url = config.API_URL + "auth/confirmation-code";
   let csrf_token = getCookie("csrf_token");
+  console.log(csrf_token);
+  console.log({ email: email });
   let res = await fetch(url, {
     method: "POST",
     credentials: "include",
@@ -12,5 +14,6 @@ export const requestConfirmCode = async (email: string) => {
     },
     body: JSON.stringify({ email: email }),
   });
+  console.log(await res.json());
   return res.ok;
 };
