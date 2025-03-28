7a915d749ef9785cd99acf7435bb22195b5d230f
George <demina.ivann@gmail.com>
2025-03-28 10:14:37 -0700

    Remove debug console.logs because issue is already resolved

--- src/helpers/request-confirmation-code.ts
+++ src/helpers/request-confirmation-code.ts
@@ -3,8 +3,6 @@ import { getCookie } from "./cookies";
 export const requestConfirmCode = async (email: string) => {
   let url = config.API_URL + "auth/confirmation-code";
   let csrf_token = getCookie("csrf_token");
-  console.log(csrf_token);
-  console.log({ email: email });
   let res = await fetch(url, {
     method: "POST",
     credentials: "include",
@@ -14,6 +12,5 @@ export const requestConfirmCode = async (email: string) => {
     },
     body: JSON.stringify({ email: email }),
   });
-  console.log(await res.json());
   return res.ok;
 };
